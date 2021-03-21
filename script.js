99e35cdaa7d5e23974e0e59e0709a98c80d7d13e
Nancy <nancyrosas0@gmail.com>
2021-03-21 15:39:54 -0500

    added buttons and local storage

--- script.js
+++ script.js
@@ -10,19 +10,29 @@ $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
 //save the text areas to local storage
 var tasksBtn = document.querySelector(".saveBtn").addEventListener("click", function() {
-    var taskEl = $("#tasks");
-    localStorage.setItem("tasks", taskEl.val());
-    //console.log(taskEl.val());
+    var taskEl = $(".tasks");
+    var time = this.value;
+    var task = taskEl.val();
+    console.log(task);
+    localStorage.setItem(time, task);
+    console.log(taskEl.val());
 
 });
 
-var savedTask = localStorage.getItem("tasks")
-console.log(savedTask);
+// var savedTask = localStorage.getItem("tasks")
+// console.log(savedTask);
+
+$("#7am-text").val(localStorage.getItem("7am"));
+$("#8am-text").val(localStorage.getItem("8am"));
+$("#9am-text").val(localStorage.getItem("9am"));
+$("#10am-text").val(localStorage.getItem("10am"));
+$("#11am-text").val(localStorage.getItem("11am"));
+$("#12pm-text").val(localStorage.getItem("12pm"));
+$("#1pm-text").val(localStorage.getItem("1pm"));
+$("#2pm-text").val(localStorage.getItem("2pm"));
+$("#3pm-text").val(localStorage.getItem("3pm"));
+
 //array of elements for 7-3 and for each
-
-
-
-
 
 
 //when we refresh, the text must remain
